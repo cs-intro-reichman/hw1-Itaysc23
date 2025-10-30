cf4f6bdea08f2fb8eefa59ff31534a3b232725a5
Itaysc23 <itay.schwartz@post.runi.ac.il>
2025-10-30 17:58:47 +0200

    fix

--- FVCalc.java
+++ FVCalc.java
@@ -1,12 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		int currentAmount = Integer.parseInt(args[0]);
-		double ratePercent = Double.parseDouble(args[1]);
-		double rate = ratePercent / 100.0;
-		int years = Integer.parseInt(args[2]);
-		double futureValue = currentAmount * Math.pow((1 + rate), years);
-		System.out.println("After " + years + " years at " + ratePercent + "%, the investment will be worth " + futureValue + " Shekels.");
+		int currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n =Integer.parseInt(args[2]);
+		int futureValue = (int) (currentValue*Math.pow(1+(rate/100), (double)n));
+		System.out.println("After " + n + " years, a $" + currentValue + " saved at " + rate + "% will yield $" + futureValue);
 
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -5,7 +5,6 @@ public class NumWords {
 		int hundreds = number / 100;
 		int tens = (number % 100) / 10;
 		int ones = number % 10;
-		System.out.println("The number " + number + " consists of:");
-		System.out.println(hundreds + " hundreds" + "," + tens + " tens" + "," + ones + " ones");
+		System.out.println(hundreds + " hundreds" + ", " + tens + "tens," + " and" + ones + "ones.");
 	}
 }
